e7542231d2b4acf78f6875687743ff940072b3b6
William Holbrook <wdh961@student.bham.ac.uk>
2022-11-04 14:39:55 +0000

    Set up django app and configure Card and List models

--- src/analyst_report_summarizer/analyst_report_summarizer/settings.py
+++ src/analyst_report_summarizer/analyst_report_summarizer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'summarizer_web_app'
 ]
 
 MIDDLEWARE = [
---
+++ src/analyst_report_summarizer/summarizer_web_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SummarizerWebAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'summarizer_web_app'
