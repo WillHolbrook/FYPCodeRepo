037ef9e7b4bb7d21df2f2536a13c92f92fa7c7ad
William Holbrook <wdh961@student.bham.ac.uk>
2022-12-06 11:05:16 +0000

    Refactor code into functions

--- src/analyst_report_extractor/main.py
+++ src/analyst_report_extractor/main.py
@@ -1,9 +1,10 @@
-from grobid_client.grobid_client import GrobidClient
-import xml.etree.ElementTree as ET
-from xml.etree.ElementTree import Element, ElementTree
 import re
-
+import xml.etree.ElementTree as ET
+from datetime import datetime
 from pathlib import Path
+from xml.etree.ElementTree import Element
+
+from grobid_client.grobid_client import GrobidClient
 
 
 # TODO add a return value probably a list of paths of outputted files
@@ -45,48 +46,63 @@ def extract_text_from_xml(input_filepath: Path, output_filepath: Path, write_mod
         f.write(text)
 
 
-def main():
-    # TODO add some handling around the grobid server being down
-    import re
-    from datetime import datetime
-
-    # client: GrobidClient = GrobidClient(config_path="./grobid_config.json")
-    root_folder_path = Path("../../FilestoreRepo/FTSE100Info/reports").resolve()
-    # root_output_folder_path = Path("../../FilestoreRepo/FTSE100Info/teixmlreports").resolve()
-    # root_txt_output_folder_path = Path("../../FilestoreRepo/FTSE100Info/txtReports").resolve()
-    # if not root_output_folder_path.exists():
-    #     root_output_folder_path.mkdir()
-    #
-    # for input_folder_path in root_folder_path.glob("*"):
-    #     if input_folder_path.is_dir():
-    #         tei_xml_output_folder_path = root_output_folder_path.joinpath(f"./{input_folder_path.name}")
-    #         if not tei_xml_output_folder_path.is_dir():
-    #             tei_xml_output_folder_path.mkdir()
-    #         print(input_folder_path, tei_xml_output_folder_path)
-    #
-    #         extract_tei_xml_from_pdf(input_folder_path, tei_xml_output_folder_path, client)
-    # count = 0
-    #
-    # for file in root_output_folder_path.rglob("*"):
-    #     if file.is_file():
-    #         count += 1
-    #
-    # print(f"Number of Reports: {count}")
-    #
-    # for input_folder_path in root_output_folder_path.glob("*"):
-    #     if input_folder_path.is_dir():
-    #         txt_output_folder_path = root_txt_output_folder_path.joinpath(f"./{input_folder_path.name}")
-    #         if not txt_output_folder_path.is_dir():
-    #             txt_output_folder_path.mkdir()
-    #         print(f"Extracting from folder {txt_output_folder_path}")
-    #
-    #         for filepath in input_folder_path.rglob("*.tei.xml"):
-    #             extract_text_from_xml(filepath, txt_output_folder_path.joinpath(f"./{filepath.name}.txt"))
-
+def print_datetime_name_and_paths_tab_separated(root_folder_path: Path):
     for input_folder_path in root_folder_path.rglob("*.pdf"):
         datetime_str = re.sub(r"(\d+)_.*", r"\1", input_folder_path.name)
         datetime_obj = datetime.strptime(datetime_str, "%Y%m%d")
         print(f"{input_folder_path}\t{input_folder_path.name}\t{datetime_obj}")
+
+
+# TODO add some handling around the grobid server being down
+def extract_tei_xml_from_root(root_output_folder_path: Path, root_input_folder_path: Path,
+                              grobid_client: GrobidClient) -> None:
+    if not root_output_folder_path.exists():
+        root_output_folder_path.mkdir()
+
+    for input_folder_path in root_input_folder_path.glob("*"):
+        if input_folder_path.is_dir():
+            tei_xml_output_folder_path = root_output_folder_path.joinpath(f"./{input_folder_path.name}")
+            if not tei_xml_output_folder_path.is_dir():
+                tei_xml_output_folder_path.mkdir()
+            print(input_folder_path, tei_xml_output_folder_path)
+
+            extract_tei_xml_from_pdf(input_folder_path, tei_xml_output_folder_path, grobid_client)
+
+
+def extract_txt_from_root(root_output_folder_path: Path, root_input_folder_path: Path):
+    for input_folder_path in root_output_folder_path.glob("*"):
+        if input_folder_path.is_dir():
+            txt_output_folder_path = root_input_folder_path.joinpath(f"./{input_folder_path.name}")
+            if not txt_output_folder_path.is_dir():
+                txt_output_folder_path.mkdir()
+            print(f"Extracting from folder {txt_output_folder_path}")
+
+            for filepath in input_folder_path.rglob("*.tei.xml"):
+                extract_text_from_xml(filepath, txt_output_folder_path.joinpath(f"./{filepath.name}.txt"))
+
+
+def get_sub_file_count(root_folder_path: Path) -> int:
+    count = 0
+
+    for file in root_folder_path.rglob("*"):
+        if file.is_file():
+            count += 1
+
+    return count
+
+
+def main():
+    client: GrobidClient = GrobidClient(config_path="./grobid_config.json")
+    root_folder_path = Path("../../FilestoreRepo/FTSE100Info/reports").resolve()
+    root_tei_xml_output_folder_path = Path("../../FilestoreRepo/FTSE100Info/teixmlreports").resolve()
+    root_txt_output_folder_path = Path("../../FilestoreRepo/FTSE100Info/txtReports").resolve()
+
+    count = get_sub_file_count(root_txt_output_folder_path)
+    # print(f"Number of Reports: {count}")
+    # print_datetime_name_and_paths_tab_separated(root_folder_path)
+    #
+    # extract_tei_xml_from_root(root_tei_xml_output_folder_path, root_folder_path, client)
+    # extract_txt_from_root(root_txt_output_folder_path, root_tei_xml_output_folder_path)
 
 
 if __name__ == "__main__":
