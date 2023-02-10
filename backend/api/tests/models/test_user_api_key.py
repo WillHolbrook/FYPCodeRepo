b15c0551dd44e68a45b994cb9dbd489ba2a071cd
William Holbrook <wdh961@student.bham.ac.uk>
2023-02-10 12:22:38 +0000

    add unimplemented tests for UserAPIKey model

---
+++ backend/api/tests/models/test_user_api_key.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Module for testing UserAPIKey"""
+from django.test import TestCase
+
+# from django.contrib.auth.models import User
+# from api.models.profile import Profile
+# from api.models.user_api_key import UserAPIKey
+
+
+class UserAPIKeyTestCase(TestCase):
+    """Tests for user API key"""
+
+    # TODO
+    def test_api_key_deletion_with_profile(self):
+        """Test for API keys getting deleted with associated profile"""
+
+    # TODO
+    def test_api_key_creation_without_specified_key_number(self):
+        """Test for API keys getting created with appropriate key number when not specified"""
+
+    # TODO
+    def test_duplicate_key1_generation(self):
+        """Test for attempting to generate a duplicate key1"""
+
+    # TODO
+    def test_duplicate_key2_generation(self):
+        """Test for attempting to generate a duplicate key1"""
+
+    # TODO
+    def test_key1_regen(self):
+        """Test for regenerating key1"""
+
+    # TODO
+    def test_key2_regen(self):
+        """Test for regenerating key2"""
