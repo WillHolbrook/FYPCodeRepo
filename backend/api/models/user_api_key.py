ed6750910ffc22e03a21841149b0567e690d33af
William Holbrook <wdh961@student.bham.ac.uk>
2023-02-13 09:46:49 +0000

    Add code to pass key regeneration tests

--- backend/api/models/user_api_key.py
+++ backend/api/models/user_api_key.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Module for UserAPIKey Model and associated methods"""
+from api.managers.user_api_key_manager import UserAPIKeyManager
 from api.models.profile import Profile
 from django.core.exceptions import ValidationError
 from django.db import models
@@ -9,11 +10,16 @@ from rest_framework_api_key.models import AbstractAPIKey
 class UserAPIKey(AbstractAPIKey):
     """Custom API key for a User"""
 
+    objects = UserAPIKeyManager()
+
+    max_num_api_keys: int = 2
+    regen: bool = False
+
     class Meta(AbstractAPIKey.Meta):
         verbose_name = "User API key"
         verbose_name_plural = "User API keys"
 
-    is_key1 = models.BooleanField(default=True)
+    is_key1 = models.BooleanField(null=True)
 
     user_profile = models.ForeignKey(
         Profile,
@@ -25,12 +31,13 @@ class UserAPIKey(AbstractAPIKey):
         if UserAPIKey.objects.filter(
             user_profile=self.user_profile, is_key1=self.is_key1
         ).exists():
-            key_num = 2 - int(bool(self.is_key1))
+            key_num = self.max_num_api_keys - int(bool(self.is_key1))
             raise ValidationError(
                 f"A single user can't have multiple API Keys for Key{key_num} "
                 f"if you want to have another value for Key{key_num} delete the "
                 f"old key before creating the new one"
             )
+        super().clean()
 
     def save(self, *args, **kwargs):
         self.full_clean()
