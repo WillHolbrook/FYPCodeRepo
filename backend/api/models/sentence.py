b011c337dcbdb8374cfa21509a87838cf7b46c25
WillHolbrook <67469852+WillHolbrook@users.noreply.github.com>
2023-02-19 16:36:04 +0000

    21 implement sentence model (#27)
    * Add sentence tokenize method
    
    * Add missing plaintext field to report model
    
    * Add create_without_file() function to create a dummy report for testing purposes
    
    * Add Sentence model
    
    * Add extra test files
    
    * Add blank functions for testing sentences
    
    * add migrations
    
    * Add TermIDF model
    
    * Add Sentence Model to admin page
    
    * Update test.plaintext to make sentence tokenization easier
    
    * add SentenceManager
    
    * Implement Sentence tests
    
    * Add Sentence Database Migrations

--- backend/api/models/sentence.py
+++ backend/api/models/sentence.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Module to contain the Sentence Model and associated methods"""
+from api.managers.sentence_manager import SentenceManager
 from api.models.report import Report
 from django.db import models
 
@@ -7,7 +8,9 @@ from django.db import models
 class Sentence(models.Model):
     """Model to represnt Sentences extracted from reports"""
 
-    parent_file = models.ForeignKey(
+    objects = SentenceManager()
+
+    parent_report = models.ForeignKey(
         Report, on_delete=models.CASCADE, related_name="sentences"
     )
 
