290a04bea3d152c3a08b47205955643bb46c6ea3
William Holbrook <wdh961@student.bham.ac.uk>
2023-02-19 16:17:44 +0000

    Add Sentence Model to admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,12 +2,14 @@
 """Used to control what appears in the django admin console"""
 from api.models.profile import Profile
 from api.models.report import Report
+from api.models.sentence import Sentence
 from api.models.user_api_key import UserAPIKey
 from django.contrib import admin
 from rest_framework_api_key.admin import APIKeyModelAdmin
 
 admin.site.register(Profile)
 admin.site.register(Report)
+admin.site.register(Sentence)
 
 
 @admin.register(UserAPIKey)
