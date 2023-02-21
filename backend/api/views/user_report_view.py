fd986f3b7894d97b051c145bd24ae032adc65eaf
William Holbrook <wdh961@student.bham.ac.uk>
2023-02-21 13:32:12 +0000

    Implement Destroy method for Report View

--- backend/api/views/user_report_view.py
+++ backend/api/views/user_report_view.py
@@ -3,13 +3,14 @@
 from api.models.report import Report
 from api.serializers.user_report_details_serializer import ReportDetailSerializer
 from api.serializers.user_report_list_serializer import ReportListSerializer
-from rest_framework import viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class ReportViewSet(
-    viewsets.ReadOnlyModelViewSet
+    viewsets.ReadOnlyModelViewSet, mixins.DestroyModelMixin
 ):  # pylint: disable=too-many-ancestors
     """Reports ViewSet for end Users"""
 
@@ -32,3 +33,10 @@ class ReportViewSet(
             return self.action_serializers.get(self.action, self.serializer_class)
 
         return super().get_serializer_class()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user == instance.user:
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
