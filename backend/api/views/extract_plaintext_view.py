b1c43aafcd0cd0aa3ec9eea9c568cdba3981b393
William Holbrook <wdh961@student.bham.ac.uk>
2023-02-22 14:29:09 +0000

    Implement extract plaintext view

---
+++ backend/api/views/extract_plaintext_view.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Module for ExtractPlaintextView"""
+from api.serializers.user_report_details_serializer import ReportDetailSerializer
+from api.views.user_report_extract_sentence_view import UserReportExtractSentenceView
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class ExtractPlaintextView(APIView):
+    """View to add (re)extract plaintext from a given report"""
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, report_pk):
+        """
+        Method executed when a post request is received
+        to rextract the report plaintext
+
+        Args:
+            request:
+            report_pk: The Primary Key of the Report to extract
+
+        Returns:
+            the updated file
+        """
+
+        (
+            response,
+            report,
+        ) = UserReportExtractSentenceView.check_user_is_present_and_has_access_to_given_report(
+            request, report_pk
+        )
+        if report is None:
+            return response
+
+        # Extract the plaintext to make sure it's up-to-date
+        report.extract_plaintext()
+
+        return Response(ReportDetailSerializer(report).data, status=status.HTTP_200_OK)
