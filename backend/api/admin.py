52331530e08091dc9fd6e1c602f0d3376a4e49cd
William Holbrook <wdh961@student.bham.ac.uk>
2023-02-26 16:32:08 +0000

    Add TermIDF model to admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,6 +3,7 @@
 from api.models.profile import Profile
 from api.models.report import Report
 from api.models.sentence import Sentence
+from api.models.term_idf import TermIDF
 from api.models.user_api_key import UserAPIKey
 from django.contrib import admin
 from rest_framework_api_key.admin import APIKeyModelAdmin
@@ -10,6 +11,7 @@ from rest_framework_api_key.admin import APIKeyModelAdmin
 admin.site.register(Profile)
 admin.site.register(Report)
 admin.site.register(Sentence)
+admin.site.register(TermIDF)
 
 
 @admin.register(UserAPIKey)
