dac2fbcdae9ff8475bdf90397cd532b581476ea6
William Holbrook <wdh961@student.bham.ac.uk>
2023-02-28 13:00:51 +0000

    add method for extracting sentence_tf_idf_weight based on sum weights

--- backend/api/managers/sentence_manager.py
+++ backend/api/managers/sentence_manager.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 """Module for SentenceManager"""
+from typing import Dict
+
+from api.grobid_tfidf_code.experiments import flatten_num_tokens, preprocess_text
 from api.grobid_tfidf_code.main import split_into_sentences
+from api.models.term_idf import TermIDF
 from django.db.models import QuerySet
 from django.db.models.manager import Manager
 from django.utils import timezone
 
 
-class SentenceManager(Manager):  # pylint: disable=too-few-public-methods
+class SentenceManager(Manager):
     """Manager for creation of Sentence objects"""
 
-    def create(
-        self, report, sentence_text: str, tf_idf_weight: float, *args, **kwargs
-    ):  # pylint: disable=unused-argument
+    def __init__(self, *args, **kwargs):
+        self.tf_idf_dict: Dict[str, float] = {}
+        super().__init__(*args, **kwargs)
+
+    def create(self, report, sentence_text: str, tf_idf_weight: float, *args, **kwargs):
         """Method to create a Sentence model from its text"""
         return super().create(
             *args,
@@ -21,12 +27,17 @@ class SentenceManager(Manager):  # pylint: disable=too-few-public-methods
             **kwargs
         )
 
-    # TODO # pylint: disable=fixme
-    @staticmethod
-    def calculate_sentence_tfidf_weight(
-        report, sentence: str  # pylint: disable=unused-argument
-    ) -> float:
+    def calculate_sentence_tfidf_weight(self, sentence: str) -> float:
         """Method used to calculate sentence_tfidf_weight for a given sentence in a given report"""
+        tokens = preprocess_text(sentence, as_list=True)
+        sentence_weight = 0.0
+        for token in tokens:
+            sentence_weight += self.tf_idf_dict.get(token, 0.0)
+
+        # if tokens:
+        #     return sentence_weight / len(tokens)
+        if tokens:
+            return sentence_weight
         return 0.0
 
     def extract_sentences(self, report) -> QuerySet:
@@ -39,14 +50,21 @@ class SentenceManager(Manager):  # pylint: disable=too-few-public-methods
         Returns:
             QuerySet of Sentence models created
         """
+        self.tf_idf_dict = {}
+
         report.sentence_datetime = timezone.now()
         report.save()
         sentences = split_into_sentences(report.plaintext)
+        report_tokens = preprocess_text(report.plaintext, as_list=True)
+        term_frequency_dict = flatten_num_tokens([report_tokens])
+        for term, frequency in term_frequency_dict.items():
+            self.tf_idf_dict[term] = frequency * TermIDF.objects.get(term=term).idf
+
         for sentence in sentences:
             self.create(
                 report=report,
                 sentence_text=sentence,
-                tf_idf_weight=self.calculate_sentence_tfidf_weight(report, sentence),
+                tf_idf_weight=self.calculate_sentence_tfidf_weight(sentence),
             )
 
         return report.sentences.all()
