8495571b381700c113732d9df424c9bd6e0ecd48
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-03 20:54:15 +0000

    Add methods in api-service.js to call django backend

--- frontend/src/api-service.js
+++ frontend/src/api-service.js
@@ -24,4 +24,52 @@ export class API {
       },
     });
   }
+
+  // TODO add passing file
+  static uploadReport() {
+    return axapi.post("api/report_upload/");
+  }
+
+  static listReports() {
+    return axapi.get("api/reports/");
+  }
+
+  static getReport(report_id) {
+    return axapi.get(`api/reports/${report_id}/`);
+  }
+
+  static deleteReport(report_id) {
+    return axapi.delete(`api/reports/${report_id}/`);
+  }
+
+  static extractSentences(report_id) {
+    return axapi.post(`api/report_extract_sentence/${report_id}/`);
+  }
+
+  static retrieveSentences(report_id, limit = 10, offset = 0) {
+    return axapi.get(
+      `api/report_extract_sentence/${report_id}/?limit=${limit}&offset=${offset}`
+    );
+  }
+
+  static getProfileDetails() {
+    return axapi.get(`api/profile/`);
+  }
+
+  // TODO add passing profile image
+  static updateProfileDetails() {
+    return axapi.post(`api/profile/`);
+  }
+
+  static listUsers() {
+    return axapi.get(`api/users/`);
+  }
+
+  static getCurrentUser() {
+    return axapi.get(`api/user/`);
+  }
+
+  static getSpecificUser(user_id) {
+    return axapi.get(`api/users/${user_id}/`);
+  }
 }
