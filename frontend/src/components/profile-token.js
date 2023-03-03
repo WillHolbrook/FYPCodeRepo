10d93805b94d78f31f02555211539021fbc05bda
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-03 20:12:12 +0000

    Add profile-token.js to nav-bar.js

--- frontend/src/components/profile-token.js
+++ frontend/src/components/profile-token.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useCookies} from "react-cookie";
+import {axapi} from "../api-service";
 
-function ProfileToken() {
+function ProfileToken(props) {
+  const [token] = useCookies(["rs_token"]);
+  const [username, setUsername] = useState("");
+  const [profileImageUrl, setProfileImageUrl] = useState("");
+
+  useEffect(() => {
+    if (token.rs_token) {
+
+    }
+  }, [token]);
+
   return (
     <div className={"profile-token"}>
-      <div className={"username"}>{}</div>
+      <div className={"profile-token-username"}>{props.username}</div>
+      <img className={"profile-picture"} src={props.profileImageUrl} alt={"Profile"}/>
     </div>
   );
 }
