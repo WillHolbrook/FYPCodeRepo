fd5f0562a91f4f6cf5aeb161d6d79557464a76ff
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-03 21:30:12 +0000

    Make profile call api for updated information

--- frontend/src/components/profile-token.js
+++ frontend/src/components/profile-token.js
@@ -1,26 +1,50 @@
+import { API, axapi } from "../api-service";
 import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 
 function ProfileToken(props) {
   const [token] = useCookies(["rs_token"]);
-  // eslint-disable-next-line no-unused-vars
-  const [username, setUsername] = useState("");
-  // eslint-disable-next-line no-unused-vars
-  const [profileImageUrl, setProfileImageUrl] = useState("");
+  const [username, setUsername] = useState(null);
+  const [profileImageUrl, setProfileImageUrl] = useState(
+    "%PUBLIC_URL%/logo512.png"
+  );
+  const max_username_length = 16;
 
   useEffect(() => {
     if (token.rs_token) {
+      API.getCurrentUser().then((resp) => {
+        if (resp.status === 200) {
+          if (resp.data.username.length > max_username_length) {
+            setUsername(
+              resp.data.username.substring(0, max_username_length - 3) + "..."
+            );
+          } else {
+            setUsername(resp.data.username);
+          }
+          setProfileImageUrl(
+            `${
+              axapi.defaults.baseURL
+            }${resp.data.profile.profile_image.substring(1)}`
+          );
+        } else {
+          console.log("Error", resp);
+        }
+      });
     }
   }, [token]);
 
   return (
     <div className={"profile-token"}>
-      <div className={"profile-token-username"}>{props.username}</div>
-      <img
-        className={"profile-picture"}
-        src={props.profileImageUrl}
-        alt={"Profile"}
-      />
+      {username ? (
+        <React.Fragment>
+          <div className={"profile-token-username"}>{username}</div>
+          <img
+            className={"profile-picture"}
+            src={profileImageUrl}
+            alt={"Profile"}
+          />
+        </React.Fragment>
+      ) : null}
     </div>
   );
 }
