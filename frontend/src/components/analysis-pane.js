6362b234f615e267cee4f8b118a02c2fd47d1196
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-04 19:22:09 +0000

    add basic analysis-pane.js

---
+++ frontend/src/components/analysis-pane.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AnalysisPane(props) {
+  return (
+    <React.Fragment>
+      <header className={"App-header"}>
+        <h2>Analysis Will Appear Here</h2>
+      </header>
+    </React.Fragment>
+  );
+}
+
+export default AnalysisPane;
