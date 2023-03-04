78432e50cebf73c117b59fbb8bb973575b2db27a
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-04 15:32:10 +0000

    Add methods for updating and deleting the currently logged-in user

--- backend/api/views/my_user_view.py
+++ backend/api/views/my_user_view.py
@@ -2,6 +2,8 @@
 """Module for MyUserView"""
 from api.serializers.user_serializer import UserSerializer
 from api.views.auth_utils import check_user_not_anon
+from django.contrib.auth import get_user_model
+from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -18,3 +20,24 @@ class MyUserView(APIView):
         """Retrieves the details of the current user"""
         check_user_not_anon(request)
         return Response(UserSerializer(request.user).data)
+
+    def put(self, request):
+        """Method to update user details"""
+        check_user_not_anon(request)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user: get_user_model() = request.user
+            user.set_password(serializer.validated_data["password"])
+            user.username = serializer.validated_data["username"]
+            user.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(
+            data=serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    def delete(self, request):
+        """Method to delete a logged in user"""
+        check_user_not_anon(request)
+        request.user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
