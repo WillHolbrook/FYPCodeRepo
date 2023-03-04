bd016d1e4d2712597c1771b129f49267e599ca68
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-04 12:26:46 +0000

    Add default homepage text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,13 @@ function App(props) {
       </div>
     );
   }
+  return (
+    <div className={"App"}>
+      <header className="App-header">
+        <h1>Homepage</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
