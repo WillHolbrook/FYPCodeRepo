019642987e18bf145f27774645de99b50824bb8f
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-04 19:23:01 +0000

    add basic report-upload.js

---
+++ frontend/src/components/report-upload.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ReportUpload(props) {
+  return (
+    <React.Fragment>
+      <header className={"App-header"}>
+        <h2>Summarize Analyst Reports</h2>
+        <span>
+          Extract important sentences as a summary of the report & extract buy,
+          sell or hold
+        </span>
+      </header>
+    </React.Fragment>
+  );
+}
+
+export default ReportUpload;
