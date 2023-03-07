6be3b6c941736bbbbbf1079ec039cc8b985f36fc
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-07 11:37:49 +0000

    remove python dependency

---
+++ frontend/src/badly-formatted.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Error() {
+  return (
+    <header className={"App-header"}>
+          <h1>Error Loading Details</h1>
+      </header>
+      );
+}
+
+export default Error;
