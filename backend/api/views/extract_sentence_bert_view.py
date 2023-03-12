0fea71e498cd72aaad8029e53f5f9bb50d7650c0
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-12 15:03:38 +0000

    Add extract_sentence_bert_view.py implementation

--- backend/api/views/extract_sentence_bert_view.py
+++ backend/api/views/extract_sentence_bert_view.py
@@ -1,21 +1,44 @@
 # -*- coding: utf-8 -*-
 """Module for UserReportExtractSentenceView"""
+from analyst_report_summarizer.settings import REST_FRAMEWORK
 from api.views.auth_utils import check_user_is_present_and_has_access_to_given_report
+from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from summarizer import Summarizer
 
 
-class UserReportExtractSentenceView(APIView):
+class ReportExtractSentenceBertView(APIView):
     """View to extract sentences from a report and retrieve extracted sentences"""
 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     model = Summarizer()
 
-    def _extract_sentences_using_bert(self):
+    @staticmethod
+    def _extract_sentences_using_bert(request, report_pk):
         """Method to extract sentences using BERT"""
+        (response, report) = check_user_is_present_and_has_access_to_given_report(
+            request, report_pk
+        )
+        if report is None:
+            return response
+
+        # Extract the plaintext if it isn't already extracted
+        if report.plaintext is None:
+            report.extract_plaintext()
+
+        sentences = ReportExtractSentenceBertView.model.run(
+            report.plaintext,
+            num_sentences=REST_FRAMEWORK["PAGE_SIZE"],
+            return_as_list=True,
+        )
+        return Response(
+            data={"count": len(sentences), "results": sentences},
+            status=status.HTTP_200_OK,
+        )
 
     def post(self, request, report_pk):
         """
@@ -29,17 +52,7 @@ class UserReportExtractSentenceView(APIView):
         Returns:
             A paginated response of the extracted sentences if the user owns the given report
         """
-        (response, report) = check_user_is_present_and_has_access_to_given_report(
-            request, report_pk
-        )
-        if report is None:
-            return response
-
-        # Extract the plaintext if it isn't already extracted
-        if report.plaintext is None:
-            report.extract_plaintext()
-
-        return None
+        return self._extract_sentences_using_bert(request, report_pk)
 
     def get(self, request, report_pk):
         """
@@ -53,10 +66,4 @@ class UserReportExtractSentenceView(APIView):
         Returns:
             A paginated response of the sentences if the user owns the given report
         """
-        (response, report) = check_user_is_present_and_has_access_to_given_report(
-            request, report_pk
-        )
-        if report is None:
-            return response
-
-        return None
+        return self._extract_sentences_using_bert(request, report_pk)
