1eaf2826996391ed9424a0f1c24829ccbab667cc
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-12 21:36:07 +0000

    Add history page

---
+++ frontend/src/components/history/history.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UploadHistory() {
+  return (
+    <div className={"App"}>
+      <header className={"App-header"}>
+        <h2>Upload History</h2>
+      </header>
+    </div>
+  );
+}
+
+export default UploadHistory;
