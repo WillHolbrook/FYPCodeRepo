8882e469f0b5ec3f3605cbbae342fe09b744a828
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-14 23:55:58 +0000

    Add caching in www directory

--- backend/api/grobid_tfidf_code/__init__.py
+++ backend/api/grobid_tfidf_code/__init__.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 """Module to make sure punkt dataset is installed"""
+import logging
+import os
+
 import nltk
 from nltk.corpus import stopwords
 
-nltk.download("punkt", quiet=True)
-nltk.download("stopwords", quiet=True)
+logger = logging.getLogger("django")
+download_path = os.getenv("NLTK_DATA_DIR", default=None)
+logger.info("Download path is %s", download_path)
+
+nltk.download("punkt", quiet=True, download_dir=download_path)
+nltk.download("stopwords", quiet=True, download_dir=download_path)
 
 STOPWORDS = stopwords.words("english")
