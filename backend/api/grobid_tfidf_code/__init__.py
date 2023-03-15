e7220554e4df18b677470021e64ff8dc9e48e046
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-15 00:40:45 +0000

    Add download path to nltk path

--- backend/api/grobid_tfidf_code/__init__.py
+++ backend/api/grobid_tfidf_code/__init__.py
@@ -9,6 +9,8 @@ from nltk.corpus import stopwords
 logger = logging.getLogger("django")
 download_path = os.getenv("NLTK_DATA_DIR", default=None)
 logger.info("Download path is %s", download_path)
+if download_path is not None:
+    nltk.data.path.append(download_path)
 
 nltk.download("punkt", quiet=True, download_dir=download_path)
 nltk.download("stopwords", quiet=True, download_dir=download_path)
