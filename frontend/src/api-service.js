2b301613d10623864215e183af7377281d4b175a
William Holbrook <wdh961@student.bham.ac.uk>
2023-03-27 19:39:36 +0100

    Update Dockerfile.apache to allow running locally

--- frontend/src/api-service.js
+++ frontend/src/api-service.js
@@ -5,7 +5,7 @@ import React from "react";
 export let axapi = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL
     ? process.env.REACT_APP_BASE_URL
-    : "http://127.0.0.1:8000/",
+    : "/",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
